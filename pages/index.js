579b41644fb28d997c6cdffa8624291f34c9eaba
paunchymonk <gregory@dickison.net>
2022-02-04 15:59:50 -0800

    Rearrange the landing page

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,41 @@
 import HeaderBar from "../components/HeaderBar";
 import Image from 'next/image'
 import group69 from '../public/images/group69.png'
+import search from '../public/images/search.svg'
+import { urlObjectKeys } from "next/dist/shared/lib/utils";
 
 const HomePage = () => {
     return (
         <div>
-            <HeaderBar text="latah christian directory"/>
-            <div className="flex p-12">
-                <div className="w-3/4">
-                    <div className="flex">
-                        <div className="w-2/3 p-20 flex flex-col space-y-8">
-                            <h1 className="font-display text-5xl">Connecting Christian Businesses</h1>
-                            <p className="font-body text-xl">
-                                Welcome to the Latah Christian Directory! Use the menu bar to the right to search for a person, organization, or service, or simply scroll down.
-                            </p>
-                        </div>
-                        <div className="w-1/3 p-20 flex justify-center items-center">
+            <HeaderBar text="palouse free market"/>
+            <div className="w-full flex justify-around h-[450px] bg-gray-100">
+                <div className="flex flex-col space-y-8 justify-center items-center">
+                    <h1 className="font-display text-6xl">Palouse Free Market</h1>
+                    <p className="font-body text-xl">
+                        Blessing brothers through business
+                    </p>
+                </div>
+                <div className="flex items-center">
+                    <input type="text" className="border border-gray-300 w-96 h-14 px-6 rounded-md shadow-md" placeholder="Search by service or busness name" />
+                </div>
+                <div className="flex">
+                <div className="flex justify-center items-center w-96">
                             <Image src={group69}/>
                         </div>
-                    </div>
-                    <div className="pl-20 w-5/6">
-                        <h1 className="font-display text-2xl py-6">Principles for Doing Business with Brothers</h1>
-                        <div className="flex flex-col space-y-6">
-                            <p className="text-xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, ipsam, ipsa nam, consequatur aut possimus nemo omnis enim qui quae amet accusamus! Qui amet sed explicabo culpa, quo voluptatum dolores!</p>
-                            <p className="text-xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, ipsam, ipsa nam, consequatur aut possimus nemo omnis enim qui quae amet accusamus! Qui amet sed explicabo culpa, quo voluptatum dolores!</p>
-                            <p className="text-xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, ipsam, ipsa nam, consequatur aut possimus nemo omnis enim qui quae amet accusamus! Qui amet sed explicabo culpa, quo voluptatum dolores!</p>
-                        </div>
-                    </div>
                 </div>
-                <div className="w-1/4 ">
-                    <input type="text" className="bg-gray-100 w-72 h-12 px-6" placeholder="Search" />
-                    <ul className="text-blue-500">
-                        <li>ACCS</li>
-                        <li>ACCS</li>
-                        <li>ACCS</li>
-                        <li>ACCS</li>
-                    </ul>
+            </div>
+            <div className="text-center bg-gray-100 h-40">
+                <p className="text-3xl">Want to add your business?</p>
+                <button type="submit" className="h-12 w-56 px-6 my-4 rounded-md shadow-lg bg-sky-700 text-blue-50 text-2xl uppercase tracking-wide">Apply Now</button>
+            </div>
+            <div className="flex p-8">
+                <div className="pl-20 w-5/6">
+                    <h1 className="font-display text-3xl py-4">Principles for Doing Business with Brothers</h1>
+                    <div className="flex flex-col space-y-6">
+                        <p className="text-xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, ipsam, ipsa nam, consequatur aut possimus nemo omnis enim qui quae amet accusamus! Qui amet sed explicabo culpa, quo voluptatum dolores!</p>
+                        <p className="text-xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, ipsam, ipsa nam, consequatur aut possimus nemo omnis enim qui quae amet accusamus! Qui amet sed explicabo culpa, quo voluptatum dolores!</p>
+                        <p className="text-xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, ipsam, ipsa nam, consequatur aut possimus nemo omnis enim qui quae amet accusamus! Qui amet sed explicabo culpa, quo voluptatum dolores!</p>
+                    </div>
                 </div>
             </div>
         </div>

15d847818feb3349536450f985961acafd497f5f
paunchymonk <gregory@dickison.net>
2022-04-23 11:40:58 -0700

    Put search bar in hero section, adjust styles

--- pages/index.js
+++ pages/index.js
@@ -9,16 +9,16 @@ const HomePage = () => {
   return (
     <div>
       <HeaderBar text="palouse free market"/>
-      <div className="w-full flex justify-around md:justify-end md:pr-20 lg:pr-32 h-[450px] lg:h-[600px] 2xl:h-[900px] bg-gray-100" style={{ backgroundImage: "url('/images/bg-table.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
-        <div className="flex flex-col space-y-8 justify-center items-center md:items-end text-gray-100">
-          <h1 className="font-display text-6xl lg:text-7xl text-center">Palouse Free Market</h1>
-          <p className="font-body text-xl md:text-2xl lg:text-3xl text-center">
+      <div className="w-full flex flex-col justify-center items-center md:items-end md:pr-12 lg:pr-32 h-[450px] lg:h-[600px] 2xl:h-[900px] bg-gray-100" style={{ backgroundImage: "url('/images/bg-table.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
+        <div className="flex flex-col space-y-8 py-10 justify-center items-center md:items-end text-gray-100">
+          <h1 className="font-display font-normal md:font-semibold text-6xl md:text-7xl text-center">Palouse Free Market</h1>
+          <p className="font-body font-semibold lg:font-normal text-xl md:text-2xl lg:text-3xl text-center">
             Goods and Services Provided with Christian Values
           </p>
         </div>
-      </div>
-      <div className="flex justify-center bg-gray-100 py-2">
-         <input type="text" className="border border-gray-300 h-14 w-5/6 px-6 rounded-md shadow-md" placeholder="Search by service or busness name" />
+        <div className="flex justify-center md:justify-end w-full md:w-4/5 lg:w-2/3 py-2">
+          <input type="text" className="border-2 border-sky-700 bg-gray-100 h-14 w-5/6 px-6 rounded-lg shadow-md placeholder:text-center placeholder:text-md md:placeholder:text-lg lg:placeholder:text-xl placeholder:text-gray-500" placeholder="Search by service or busness name" />
+        </div>
       </div>
       <div className="text-center bg-gray-100 h-40 py-4">
         <p className="text-3xl">Want to add your business?</p>

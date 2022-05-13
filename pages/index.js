d3161421a30d57817b7e407be8408ec9b022b682
paunchymonk <gregory@dickison.net>
2022-05-12 20:24:19 -0700

    Adjust position of header elements

--- pages/index.js
+++ pages/index.js
@@ -20,15 +20,15 @@ const HomePage = () => {
   return (
     <div className="w-screen max-w-[1600px]">
       <NavBar/>
-      <div className="w-full flex flex-col justify-center items-center md:items-end md:pr-12 lg:pr-32 h-[450px] lg:h-[600px] 2xl:h-[900px] bg-gray-100" style={{ backgroundImage: "url('/images/bg-table.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
+      <div className="w-full flex flex-col justify-center items-center md:space-y-16 lg:space-y-24 md:items-end md:pr-12 lg:pr-32 h-[450px] lg:h-[600px] 2xl:h-[900px] bg-gray-100" style={{ backgroundImage: "url('/images/bg-table.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
         <div className="flex flex-col space-y-8 py-10 justify-center items-center md:items-end text-gray-100">
           <h1 className="font-display font-normal md:font-semibold text-6xl md:text-7xl text-center">Palouse Free Market</h1>
           <p className="font-body font-semibold lg:font-normal text-xl md:text-2xl lg:text-3xl text-center">
             A Listing of Goods and Services Provided with Christian Values
           </p>
         </div>
-        <div className="flex justify-center md:justify-end w-full md:w-4/5 lg:w-2/3 py-2">
-          <input id="listingFilter" type="text" className="border-2 border-sky-700 bg-gray-100 h-14 w-5/6 px-6 rounded-lg shadow-md placeholder:text-center placeholder:text-md md:placeholder:text-lg lg:placeholder:text-xl placeholder:text-gray-500" placeholder="Search by service or business name" onChange={filterList} />
+        <div className="flex justify-center md:justify-end lg:justify-start w-full md:w-4/5 lg:w-2/3 py-2">
+          <input id="listingFilter" type="text" className="border-4 border-gray-900 bg-gray-100 h-14 w-5/6 px-6 rounded-lg shadow-md placeholder:text-center placeholder:text-md md:placeholder:text-xl lg:placeholder:text-2xl placeholder:text-gray-900 opacity-70 md:opacity-60" placeholder="Search by service or business name" onChange={filterList} />
         </div>
       </div>
       <div className="text-center bg-gray-100 h-40 py-4">
